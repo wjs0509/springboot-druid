55e5f7ef3f6b4d3edbbf4f1122e7a2108a578581
wjs0509 <wjs0509@163.com>
2019-01-31 15:47:49 +0800

    增加创建时间和更新时间

--- src/main/java/com/wjs/entity/User.java
+++ src/main/java/com/wjs/entity/User.java
@@ -1,9 +1,15 @@
 package com.wjs.entity;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.*;
+import java.util.Date;
 
 //使用JPA注解配置映射关系
 @Entity//告诉JPA这是一个实体类（和数据表是映射关系）
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "user")
 public class User {
     @Id//这是一个主键
@@ -13,6 +19,28 @@ public class User {
     private String lastName;
     @Column
     private String email;
+    @Column
+    @CreatedDate
+    private Date createdDate;
+    @Column
+    @LastModifiedDate
+    private Date updatedDate;
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 
     public Integer getId() {
         return id;

33331f12006b8ee35eb51759e5bf0e460ec614fc
wjs0509 <wjs0509@163.com>
2019-01-31 15:04:59 +0800

    增加通用返回类Result

--- src/main/java/com/wjs/controller/UserController.java
+++ src/main/java/com/wjs/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.wjs.controller;
 
 import com.wjs.entity.User;
+import com.wjs.result.Result;
+import com.wjs.result.ResultUtil;
 import com.wjs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -8,38 +10,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@SuppressWarnings("unchecked")
 public class UserController {
 
     @Autowired
     UserService userService;
 
     @GetMapping("/user/{id}")
-    public User getUser(@PathVariable("id") Integer id){
-        return userService.findUser(id);
+    public Result<User> getUser(@PathVariable("id") Integer id){
+        return ResultUtil.success(userService.findUser(id));
     }
 
     @PostMapping("/user")
-    public User insetUser(@RequestBody User user){
-        return userService.saveUser(user);
+    public Result<User> insetUser(@RequestBody User user){
+        return ResultUtil.success(userService.saveUser(user));
     }
 
     @GetMapping("/getAll")
-    public List<User> getUsers(){
-        return userService.findAllUser();
+    public Result<List<User>> getUsers(){
+        return ResultUtil.success(userService.findAllUser());
     }
 
     @GetMapping("/delete/{id}")
-    public Boolean  deleteUser(@PathVariable("id") String id){
-        return userService.deleteUser(Integer.valueOf(id));
+    public Result<Boolean>  deleteUser(@PathVariable("id") String id){
+        return ResultUtil.success(userService.deleteUser(Integer.valueOf(id)));
     }
 
     @PostMapping("/update")
-    public User updateUser(Integer id,String lastName,String email){
+    public Result<User> updateUser(Integer id,String lastName,String email){
         User user = userService.findUser(id);
         user.setLastName(lastName);
         user.setEmail(email);
         userService.updateUser(user);
-        return user;
+        return ResultUtil.success(user);
     }
 }
 
---
+++ src/main/java/com/wjs/result/ResultEnum.java
@@ -0,0 +1,34 @@
+package com.wjs.result;
+
+public enum ResultEnum {
+    // 成功
+    SUCCESS(200,"成功"),
+
+    // 失败
+    FAIL(400,"失败"),
+
+    // 未认证（签名错误）
+    UNAUTHORIZED(401,"未授权"),
+
+    // 接口不存在
+    NOT_FOUND(404,"接口不存在"),
+
+    // 服务器内部错误
+    INTERNAL_SERVER_ERROR(500,"服务器内部错误");
+
+    private Integer code;
+    private String msg;
+
+    ResultEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
